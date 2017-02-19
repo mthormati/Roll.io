1383614684ce536232df5d1147483e3440903fa4
Aung Myat <amyat@purdue.edu>
2017-02-18 19:20:34 -0600

    Added scriptes and settings

---
+++ Assets/Scripts/AIController2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIController2 : MonoBehaviour {
+
+	public float acceleration;
+	public GameObject AIBall;
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start(){
+		rb = AIBall.GetComponent<Rigidbody>();
+
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 movement = new Vector3 (acceleration, 0, 0);
+		rb.AddForce (movement);
+	}
+}
---
+++ Assets/Scripts/PowerBarController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerBarController : MonoBehaviour {
+
+
+	public Image powerBar;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 slidePos = Camera.main.WorldToScreenPoint (this.transform.position);
+		powerBar.transform.position = slidePos;
+
+	}
+
+
+}
